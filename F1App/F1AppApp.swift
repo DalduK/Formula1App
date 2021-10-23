bd218c8656a063623f36131f981a510ce1866e5c
DalduK <przemyslaw7wozny@gmail.com>
2021-10-23 13:02:17 +0200

    Initial Commit

---
+++ F1App/F1AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1AppApp.swift
+//  F1App
+//
+//  Created by Przemysław Woźny on 23/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct F1AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
